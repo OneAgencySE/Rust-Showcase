b209af287f91763fdc8b7bef7f412fa2ee6332bf
Alexander Herlin <alexander.herlin@outlook.com>
2020-06-05 09:04:44 +0200

    First session 04/06-20

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod lib;
+/// Subjects: Ownership, Lifetime
+/// In the lib file we find lifetime
+/// Serialize/Deserialize
+fn main() {
+    println!(
+        r#"
+        Hello to you! 
+        Check out lib.rs for what we looked at!
+        "#
+    );
+}
